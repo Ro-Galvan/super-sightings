38c6a01834380e07b9a044a948fdc57c2f126603
Sophia Zhou <102894133+sbagel@users.noreply.github.com>
2023-07-17 11:07:17 -0700

    implement organizationDao interface, init impl

--- supersightings/src/main/java/com/sg/supersightings/dao/OrganizationDaoImpl.java
+++ supersightings/src/main/java/com/sg/supersightings/dao/OrganizationDaoImpl.java
@@ -1,4 +1,42 @@
 package com.sg.supersightings.dao;
 
-public class OrganizationDaoImpl {
+import com.sg.supersightings.dto.Organization;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public class OrganizationDaoImpl implements OrganizationDao {
+    private final JdbcTemplate jdbc;
+    @Autowired
+    public OrganizationDaoImpl(JdbcTemplate jdbcTemplate) {
+        this.jdbc = jdbcTemplate;
+    }
+    @Override
+    public List<Organization> getAllOrganizations() {
+        return null;
+    }
+
+    @Override
+    public Organization getOrganizationById(int orgId) {
+        return null;
+    }
+
+    @Override
+    public Organization addOrganization(Organization org) {
+        return null;
+    }
+
+    @Override
+    public void updateOrganization(Organization org) {
+
+    }
+
+    @Override
+    public void deleteOrganizationById(int orgId) {
+
+    }
 }
+
