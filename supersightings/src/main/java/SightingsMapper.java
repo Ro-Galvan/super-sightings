ebcc65c1fac4fe3dbe1825dba3efcc5ac6b936b1
Yuliet Hernandez <yuliethdez2020@gmail.com>
2023-07-17 11:52:10 +0100

    Adding Mapper and DTO

---
+++ supersightings/src/main/java/SightingsMapper.java
@@ -0,0 +1,20 @@
+
+import com.sg.supersightings.dto.Sighting;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.time.LocalDateTime;
+import org.springframework.jdbc.core.RowMapper;
+
+
+public class SightingsMapper implements RowMapper<Sighting>{
+
+    @Override
+    public Sighting mapRow(ResultSet rs, int rowNum) throws SQLException {
+        Sighting sighting= new Sighting();
+        sighting.setSightingID(rs.getInt("sightingID"));
+        sighting.setDateSighted(rs.getObject("dateSighted", LocalDateTime.class));
+
+        return sighting;
+    }
+    
+}
--- supersightings/src/main/java/com/sg/supersightings/dto/Sighting.java
+++ supersightings/src/main/java/com/sg/supersightings/dto/Sighting.java
@@ -1,4 +1,28 @@
 package com.sg.supersightings.dto;
 
+import java.time.LocalDateTime;
+
 public class Sighting {
+    private int sightingID;
+    private LocalDateTime dateSighted;
+
+    public int getSightingID() {
+        return sightingID;
+    }
+
+    public void setSightingID(int sightingID) {
+        this.sightingID = sightingID;
+    }
+
+    public LocalDateTime getDateSighted() {
+        return dateSighted;
+    }
+
+    public void setDateSighted(LocalDateTime dateSighted) {
+        this.dateSighted = dateSighted;
+    }
+
+   
+    
+    
 }
