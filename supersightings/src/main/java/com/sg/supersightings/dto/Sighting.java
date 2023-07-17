ccf698b288f4b1c275736f9893d6d50e253a0cf2
Yuliet Hernandez <yuliethdez2020@gmail.com>
2023-07-17 12:03:51 +0100

    Adding Mapper and DTO

--- supersightings/src/main/java/com/sg/supersightings/dto/Sighting.java
+++ supersightings/src/main/java/com/sg/supersightings/dto/Sighting.java
@@ -1,6 +1,7 @@
 package com.sg.supersightings.dto;
 
 import java.time.LocalDateTime;
+import java.util.Objects;
 
 public class Sighting {
     private int sightingID;
@@ -20,6 +21,37 @@ public class Sighting {
 
     public void setDateSighted(LocalDateTime dateSighted) {
         this.dateSighted = dateSighted;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 59 * hash + this.sightingID;
+        hash = 59 * hash + Objects.hashCode(this.dateSighted);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Sighting other = (Sighting) obj;
+        if (this.sightingID != other.sightingID) {
+            return false;
+        }
+        return Objects.equals(this.dateSighted, other.dateSighted);
+    }
+
+    @Override
+    public String toString() {
+        return "Sighting{" + "sightingID=" + sightingID + ", dateSighted=" + dateSighted + '}';
     }
 
    
