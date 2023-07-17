8cc6b6e47348051e966e3b975b1efe06e7cbc00e
ro_g <97912154+Ro-Galvan@users.noreply.github.com>
2023-07-17 15:37:00 -0400

    added super DaoImpl methods

--- supersightings/src/main/java/com/sg/supersightings/dao/SuperDao.java
+++ supersightings/src/main/java/com/sg/supersightings/dao/SuperDao.java
@@ -1,4 +1,15 @@
 package com.sg.supersightings.dao;
 
+import com.sg.supersightings.dto.Super;
+
+import java.util.List;
+
 public interface SuperDao {
+    Super createSuper(Super superCharacter);
+    List<Super> getAllSuperCharacters();
+    Super getSuperByID(int superId);
+    void updateSuper(Super superCharacter);
+//    FK association with superpower Table
+    void deleteAllSuperpowersFromSuper(int superpowerId);
+    void deleteSuper(int superId);
 }
