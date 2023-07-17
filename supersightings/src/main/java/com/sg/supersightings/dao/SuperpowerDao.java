9c0fe3291fd5311ea6386446829fb5fbd4260c89
Nathalie Surman <onepiecenat@hotmail.com>
2023-07-17 15:16:04 -0400

    Added methods in Dao for superpower

--- supersightings/src/main/java/com/sg/supersightings/dao/SuperpowerDao.java
+++ supersightings/src/main/java/com/sg/supersightings/dao/SuperpowerDao.java
@@ -1,4 +1,24 @@
 package com.sg.supersightings.dao;
 
+import com.sg.supersightings.dto.Superpower;
+
+import java.util.List;
+
 public interface SuperpowerDao {
+
+    // This method retrieves a superpower based on its ID.
+    Superpower getSuperpowerById(int superpowerId);
+
+
+    // This method retrieves a list of all available superpowers.
+    List<Superpower> getAllPowers();
+
+    // This method adds a new superpower to the system and returns the added superpower.
+    Superpower addSuperpower(Superpower superpower);
+
+    // This method updates an existing superpower with new information.
+    void  updateSuperpower(Superpower superpower);
+
+    // This method deletes a superpower based on its ID.
+    void deleteSuperpowerById(int deleteId);
 }
